1e478bf8edaa7a6e1e82d2c4d54906abc189f5ed
vosidqarshiboyev <101445824+vosidqarshiboyev@users.noreply.github.com>
2023-02-27 21:12:31 +0500

    Add files via upload
    initial commit

---
+++ Automated_testing_with_java/src/main/java/com/epam/training/student_vosid_karshiboev/fundamental/main_task/Main.java
@@ -0,0 +1,7 @@
+package com.epam.training.student_vosid_karshiboev.fundamental.main_task;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
