b8d4492db93639b08b54f6c5e770bf22b2789030
vosidqarshiboyev <101445824+vosidqarshiboyev@users.noreply.github.com>
2023-04-06 18:30:37 +0500

    Add files via upload

---
+++ gradually-decreasing-carousel/src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
@@ -0,0 +1,16 @@
+package com.epam.rd.autotasks;
+
+public class GraduallyDecreasingCarousel extends DecrementingCarousel{
+    public GraduallyDecreasingCarousel(final int capacity) {
+        super(capacity);
+    }
+
+    @Override
+    public CarouselRun run() {
+        if (!isRun) {
+            isRun = true;
+            return new GraduallyDecreasingCarouselRun();
+        }
+        return null;
+    }
+}
